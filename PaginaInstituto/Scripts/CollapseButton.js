cc7253e45a6dd5edb7f4daa58664d7796727a490
Gino Farias <fariasgino@yahoo.com>
2025-06-21 21:30:09 -0300

    style: ajustar ancho del sidebar y alineación del logo y botón

--- PaginaInstituto/Scripts/CollapseButton.js
+++ PaginaInstituto/Scripts/CollapseButton.js
@@ -1,8 +1,10 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     const toggleBtn = document.querySelector(".toggle-btn");
     const sidebar = document.getElementById("sidebar");
+    const mainContent = document.getElementById("main-content");
 
     toggleBtn.addEventListener("click", function () {
         sidebar.classList.toggle("collapsed");
+        mainContent.classList.toggle("expanded-content");
     });
 });
